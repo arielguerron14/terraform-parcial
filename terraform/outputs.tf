d8304e51f71a91743a673e7888e9e2b60b8c1f6c
Ariel Guerrón <caguerronp@uce.edu.ec>
2025-12-11 13:07:06 -0500

    trigger: deploy complete

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,8 +1,14 @@
-output "alb_dns" {
-  value = aws_lb.alb.dns_name
+output "alb_dns_name" {
+	description = "DNS name of the Application Load Balancer"
+	value       = aws_lb.alb.dns_name
+}
+
+output "vpc_id" {
+	description = "ID of the created VPC"
+	value       = aws_vpc.main.id
 }
 
 output "asg_name" {
-  value = aws_autoscaling_group.asg.name
+	description = "Name of the Auto Scaling Group"
+	value       = aws_autoscaling_group.asg.name
 }
-
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,52 +1,71 @@
 variable "region" {
-  type    = string
-  default = "us-east-1"
+	description = "AWS region to deploy into"
+	type        = string
+	default     = "us-east-1"
 }
 
 variable "vpc_cidr" {
-  type    = string
-  default = "10.0.0.0/16"
+	description = "CIDR block for the VPC"
+	type        = string
+	default     = "10.0.0.0/16"
 }
 
 variable "public_subnets" {
-  type    = list(string)
-  default = ["10.0.1.0/24", "10.0.2.0/24"]
+	description = "List of public subnet CIDRs"
+	type        = list(string)
+	default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "instance_type" {
-  type    = string
-  default = "t2.micro"
-}
-
-variable "asg_min" {
-  type    = number
-  default = 2
-}
-
-variable "asg_max" {
-  type    = number
-  default = 6
-}
-
-variable "asg_desired" {
-  type    = number
-  default = 6
+	description = "EC2 instance type"
+	type        = string
+	default     = "t3.micro"
 }
 
 variable "ssh_key_name" {
-  type        = string
-  default     = "recuperacion"
-  description = "Nombre de key pair para acceso SSH a las instancias EC2. Si se crea a partir de 'ssh_public_key', se usará este nombre."
+	description = "Name of the SSH key pair to attach (empty = none)"
+	type        = string
+	default     = ""
 }
 
 variable "ssh_public_key" {
-  type        = string
-  default     = ""
-  description = "(Opcional) Clave pública SSH. Si se provee, Terraform creará un key pair con el nombre `ssh_key_name` usando esta clave pública."
+	description = "(Optional) Public SSH key. If provided, Terraform will create a key pair using `ssh_key_name`."
+	type        = string
+	default     = ""
 }
 
 variable "existing_vpc_id" {
+	description = "(Optional) If provided, Terraform will use this VPC instead of creating a new one."
+	type        = string
+	default     = ""
+}
+
+variable "asg_max" {
+	description = "Auto Scaling Group max size"
+	type        = number
+	default     = 2
+}
+
+variable "asg_min" {
+	description = "Auto Scaling Group min size"
+	type        = number
+	default     = 1
+}
+
+variable "asg_desired" {
+	description = "Auto Scaling Group desired capacity"
+	type        = number
+	default     = 1
+}
+
+variable "ami_id" {
+  description = "AMI ID to use (optional). If empty, a data lookup will be used."
   type        = string
   default     = ""
-  description = "(Opcional) Si se proporciona, Terraform usará este VPC en lugar de crear uno nuevo."
+}
+
+variable "azs" {
+  description = "List of AZ names to use (optional). If empty, a data lookup will be used."
+  type        = list(string)
+  default     = []
 }
